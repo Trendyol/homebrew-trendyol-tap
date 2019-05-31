f4edfbc847d51f5b13f9a4760a92544ee721109a
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2019-05-31 15:03:56 +0300

    easy-rollback formulae v1.0.2

--- easy-rollback.rb
+++ easy-rollback.rb
@@ -1,27 +1,22 @@
-  # Documentation: https://docs.brew.sh/Formula-Cookbook
+# Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class EasyRollback < Formula
   desc ":recycle: Now you can easily rollback to previous deployed images whatever you want on k8s environment"
-  homepage "https://github.com/Trendyol/easy-rollback"
+  homepage ""
   url "https://github.com/Trendyol/easy-rollback/archive/v1.0.2.tar.gz"
-  sha256 "9b5ff3203569b133ab6e3789c340d9ef3212c41ee7f189e65937150fef12b1e6"
-  # depends_on "cmake" => :build
+  sha256 "6ecd737a6f1bd8d5c0fc77f85fcd8a852e8cdecf40f2f290625518332651fa2a"
   depends_on "go" => :build
 
-   def install
-        system "gobuild.sh"
-        bin.install ".gobuild/bin/easy-rollback" => "easy-rollback"
-      end
+  def install
+       system "gobuild.sh"
+       bin.install ".gobuild/bin/easy-rollback" => "easy-rollback"
+     end
 
-      test do
-        system "#{bin}/easy-rollback", "--help"
-      end
+     test do
+       system "#{bin}/easy-rollback", "--help"
+     end
 end
-
-~
-~
-~
 ~
 ~
 ~
