c7bb31cbfe61f33f78cf08bd373679453b7e29f4
mstrYoda <freelancemuhendis@gmail.com>
2020-02-01 15:53:35 +0300

    update sha256 docker-shell

--- docker-shell.rb
+++ docker-shell.rb
@@ -7,7 +7,7 @@ class DockerShell < Formula
   desc "A simple interactive prompt for docker"
   homepage "https://github.com/Trendyol/docker-shell"
   url "https://github.com/Trendyol/docker-shell/archive/v1.2.0.tar.gz"
-  sha256 "8993cb15e854b2fe97bd6193e42d6d33ab56531e6ef82fe70f174574362c1c82"
+  sha256 "aeb3d88e27d0567b271eb0f250fa21c888193910d0e255063a87cd4bd9f3f50e"
   head "https://github.com/Trendyol/docker-shell"
 
   depends_on "go" => :build
