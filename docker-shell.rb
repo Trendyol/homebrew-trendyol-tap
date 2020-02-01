e0666feb07f273f23c24de785baa6e7208839244
mstrYoda <freelancemuhendis@gmail.com>
2020-02-01 15:52:35 +0300

    add v1.2.0 to docker-shell

--- docker-shell.rb
+++ docker-shell.rb
@@ -6,7 +6,7 @@ require "language/go"
 class DockerShell < Formula
   desc "A simple interactive prompt for docker"
   homepage "https://github.com/Trendyol/docker-shell"
-  url "https://github.com/Trendyol/docker-shell/archive/v1.1.0.tar.gz"
+  url "https://github.com/Trendyol/docker-shell/archive/v1.2.0.tar.gz"
   sha256 "8993cb15e854b2fe97bd6193e42d6d33ab56531e6ef82fe70f174574362c1c82"
   head "https://github.com/Trendyol/docker-shell"
 
