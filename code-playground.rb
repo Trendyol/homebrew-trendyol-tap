4a672247e4512a12e52661b5a01538dd1b08a706
Hüseyin Güner <hus.gunr@gmail.com>
2020-02-13 13:09:09 +0300

    Update code-playground.rb

--- code-playground.rb
+++ code-playground.rb
@@ -7,28 +7,20 @@ class CodePlayground < Formula
   url "https://github.com/Trendyol/code-playground/archive/v1.0.0.tar.gz"
   sha256 "2ccd7992b817fd44f2abe3bc44af04af7b77f1314d756ca3e5866b1acf34b3c4"
 
-  # depends_on "cmake" => :build
+  depends_on "go" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+
+    ENV["GOPATH"] = buildpath
+    bin_path = buildpath/"src/github.com/Trendyol/code-playground"
+    bin_path.install Dir["*"]
+
+    cd bin_path do
+      system "go", "build", "-o", bin/"code-playground", "."
+    end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test code-playground`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end
