dbc25c6a79896489e95ea6334522b590f3b9b7b1
Hüseyin Güner <hus.gunr@gmail.com>
2020-04-12 14:52:59 +0300

    Update verg.rb

--- verg.rb
+++ verg.rb
@@ -4,23 +4,14 @@
 class Verg < Formula
   desc "verG is a semantic versioning CLI"
   homepage "https://github.com/Trendyol/verg/"
-  url "https://github.com/Trendyol/verg/archive/v0.1.0.tar.gz"
-  sha256 "0959bcafc72011c1532c8705ddd0f9461722311558c62f614e033890fd677ae6"
-
-  depends_on "go" => :build
+  url "https://github.com/Trendyol/verg/archive/v0.3.0.tar.gz"
+  sha256 "f93082f9692b25423754bfb5cc95b0a0371814aa3c339afe968f0c593caab5bd"
 
   def install
-
-    ENV["GOPATH"] = buildpath
-    bin_path = buildpath/"src/github.com/Trendyol/verg"
-    bin_path.install Dir["*"]
-
-    cd bin_path do
-      system "go", "build", "-o", bin/"verg", "."
-    end
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/verg", "--help"
+    system "verg", "--help"
   end
 end
