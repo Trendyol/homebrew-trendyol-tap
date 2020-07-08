8ea5124f855630480bb2b71fbb27c7279fdfea1a
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2020-07-08 17:08:33 +0300

    easy-rollback updated to version 1.0.16

--- easy-rollback.rb
+++ easy-rollback.rb
@@ -7,23 +7,24 @@ class EasyRollback < Formula
   url "https://github.com/Trendyol/easy-rollback/archive/v1.0.16.tar.gz"
   sha256 "18b56225565431299b93a8770ddbcfd28b9ad28254145ac4ae74fd0e26b736f1"
   license ""
+
   depends_on "go" => :build
 
-   def install
-     ENV["GOPATH"] = buildpath
+    def install
+      ENV["GOPATH"] = buildpath
 
-     bin_path = buildpath/"src/github.com/Trendyol/easy-rollback"
-     # Copy all files from their current location (GOPATH root)
-     # to $GOPATH/src/github.com/kevinburke/hostsfile
-     bin_path.install Dir["*"]
-     cd bin_path do
-       # Install the compiled binary into Homebrew's `bin` - a pre-existing
-       # global variable
-       system "go", "build", "-o", bin/"easy-rollback", "."
-     end
+      bin_path = buildpath/"src/github.com/Trendyol/easy-rollback"
+      # Copy all files from their current location (GOPATH root)
+      # to $GOPATH/src/github.com/kevinburke/hostsfile
+      bin_path.install Dir["*"]
+      cd bin_path do
+        # Install the compiled binary into Homebrew's `bin` - a pre-existing
+        # global variable
+        system "go", "build", "-o", bin/"easy-rollback", "."
+      end
+    end
+
+    test do
+          system "#{bin}/easy-rollback", "--help"
+        end
    end
-
-   test do
-         system "#{bin}/easy-rollback", "--help"
-       end
-  end
