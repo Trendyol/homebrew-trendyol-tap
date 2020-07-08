c1c25958bbf5d674d70c7e907d7b5a11b2929ab6
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2020-07-08 17:06:58 +0300

    easy-rollback updated to version 1.0.16

--- easy-rollback.rb
+++ easy-rollback.rb
@@ -4,7 +4,7 @@
 class EasyRollback < Formula
   desc ":recycle: Now you can easily rollback to previous deployed images whatever you want on k8s environment"
   homepage ""
-  url "https://github.com/Trendyol/easy-rollback/archive/v1.0.15.tar.gz"
+  url "https://github.com/Trendyol/easy-rollback/archive/v1.0.16.tar.gz"
   sha256 "18b56225565431299b93a8770ddbcfd28b9ad28254145ac4ae74fd0e26b736f1"
   license ""
   depends_on "go" => :build
