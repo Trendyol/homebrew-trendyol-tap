119f4365f27b9a3fbb71ca2ff85e8ad0c65cad92
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2020-07-08 15:44:37 +0300

    easy-rollback updated to version 1.0.15

--- easy-rollback.rb
+++ easy-rollback.rb
@@ -4,25 +4,26 @@
 class EasyRollback < Formula
   desc ":recycle: Now you can easily rollback to previous deployed images whatever you want on k8s environment"
   homepage ""
-  url "https://github.com/Trendyol/easy-rollback/archive/v1.0.9.tar.gz"
-  sha256 "253fbf2fa81fd37d19d64a2928b99562b4f6df48c5aa698b8b72e04ada434e62"
+  url "https://github.com/Trendyol/easy-rollback/archive/v1.0.15.tar.gz"
+  sha256 "f750cd8d519522fafde1a81a8e7d265b3c5f47446f5d3d506779411761c736ea"
+  license ""
   depends_on "go" => :build
 
- def install
-   ENV["GOPATH"] = buildpath
+   def install
+     ENV["GOPATH"] = buildpath
 
-   bin_path = buildpath/"src/github.com/Trendyol/easy-rollback"
-   # Copy all files from their current location (GOPATH root)
-   # to $GOPATH/src/github.com/kevinburke/hostsfile
-   bin_path.install Dir["*"]
-   cd bin_path do
-     # Install the compiled binary into Homebrew's `bin` - a pre-existing
-     # global variable
-     system "go", "build", "-o", bin/"easy-rollback", "."
-   end
- end
-
- test do
-       system "#{bin}/easy-rollback", "--help"
+     bin_path = buildpath/"src/github.com/Trendyol/easy-rollback"
+     # Copy all files from their current location (GOPATH root)
+     # to $GOPATH/src/github.com/kevinburke/hostsfile
+     bin_path.install Dir["*"]
+     cd bin_path do
+       # Install the compiled binary into Homebrew's `bin` - a pre-existing
+       # global variable
+       system "go", "build", "-o", bin/"easy-rollback", "."
      end
-end
+   end
+
+   test do
+         system "#{bin}/easy-rollback", "--help"
+       end
+  end
